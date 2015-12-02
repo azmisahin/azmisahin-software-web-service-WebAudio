2c935c3ae59398ef7d1fdcdc7d7b18fc16af9d9c
İnstitution developer <institution-developer@outlook.com>
2015-12-02 17:14:32 +0200

    Web Audio

---
+++ WebAudio/WebAudio/Properties/AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+[assembly: AssemblyTitle("WebAudio")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("azmisahin.com")]
+[assembly: AssemblyProduct("WebAudio")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("bb35846d-c34c-4d6b-be3d-86258e83814d")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ WebAudio/WebAudio/web-audio.js
@@ -0,0 +1,27 @@
+﻿/// Web Audio
+function WebAudio() { this.init() };
+
+/// Web Audio Prototye
+WebAudio.prototype = (function () {
+
+    var context = {
+        Version: "0.0.0.1"
+        , WebAudio: new AudioContext()
+    }
+
+    /// Init
+    function Init() {
+        var result = "Web Audio Initilaize! Version : " + context.Version;
+        console.log(result);
+        return result;
+    }
+    return {
+        version: context.Version,
+        constructor: WebAudio
+        , init: function () { Init(); }
+        , context: function () { return context; }
+    }
+})();
+
+/// Application Start
+var webAudio = new WebAudio();
