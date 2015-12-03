7c1a314cad6f3b661b11b00a348e62d3db2b3b0b
İnstitution developer <institution-developer@outlook.com>
2015-12-03 16:53:22 +0200

    Play property: webAudio.play("sample/hi.wav");

--- WebAudio/WebAudio/web-audio.js
+++ WebAudio/WebAudio/web-audio.js
@@ -7,6 +7,8 @@ WebAudio.prototype = (function () {
     var context = {
         Version: "0.0.0.1"
         , WebAudio: new AudioContext()
+        , request: new XMLHttpRequest()
+        , buffer:null
     }
 
     /// Init
@@ -15,11 +17,45 @@ WebAudio.prototype = (function () {
         console.log(result);
         return result;
     }
+
+    /// Play
+    function Play(url)
+    {
+        context.request.open('get', url, true);
+        context.request.responseType = 'arraybuffer';
+        context.request.onload = requestOnload;
+        context.request.send();
+    }
+
+    // requestOnload
+    function requestOnload()
+    {
+        context.WebAudio.decodeAudioData(context.request.response, function (buffer) {
+            context.buffer = buffer;
+            playBuffer(context.buffer);
+        }, errorCallback);
+    }
+
+    // play Buffer
+    function playBuffer(buffer) {
+        var source = context.WebAudio.createBufferSource();
+        source.buffer = buffer;
+        source.connect(context.WebAudio.destination);
+        source.start(0);
+    }
+
+    // error Callback
+    function errorCallback(data) {
+        console.log(data);
+    }
+
+    /// Return Class
     return {
         version: context.Version,
         constructor: WebAudio
         , init: function () { Init(); }
         , context: function () { return context; }
+        , play: function (url) { Play(url);}
     }
 })();
 
